2e8de571a045252e7de2dbe3317b6c2200d875df
KamCy Ahaka <kamcyahaka@gmail.com>
2023-11-13 22:26:25 +0100

    ➕ Added new custom components for

---
+++ components/CustomFileInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="input-container flex flex-col gap-2 mb-3">
+    <label
+      :for="props.id"
+      class="font-semibold"
+    >{{ props.label }}
+      <span
+        v-if="props.required === 'true'"
+        class="text-red-600 font-bold"
+      >*</span>
+    </label>
+    <input
+      :type="props.type"
+      :id="props.id"
+      :name="props.name"
+      :required="true"
+      :accept="props.accepts"
+      class=""
+      :value="modelValue"
+      @input="$emit('update:modelValue', ($event.target as HTMLInputElement)?.files)"
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  label: string,
+  type: string,
+  id: string,
+  name: string,
+  modelValue?: string,
+  accepts?: string,
+  required?: 'true' | 'false'
+  placeholder?: string,
+}>()
+
+defineEmits(['update:modelValue'])
+</script>
+
+<style lang="scss" scoped>
+input {
+  outline: none;
+}
+</style>
--- components/CustomRadioInput.vue
+++ components/CustomRadioInput.vue
@@ -1,27 +1,24 @@
 <template>
-  <div :class="`${props.inputName}-input-container-wrapper flex flex-col gap-1 py-2`">
-    <div :class="`${props.inputName}-input-container-wrapper flex items-start gap-2`">
-      <CustomInput
-        v-for="radio in props.radios"
-        :id="radio.id"
-        :name="props.inputName"
-        :label="radio.label"
-        type="radio"
-        :value="radio.value"
-      />
-    </div>
-  </div>
+  <CustomInput
+    :id="props.radio.id"
+    :name="props.inputName"
+    :label="props.radio.label"
+    type="radio"
+    :value="props.radio.value"
+    @input="$emit(
+      'genderSelected', props.radio.value)"
+  />
 </template>
 
 <script setup lang="ts">
+
 const props = defineProps<{
-  containerLabel: string,
   inputName: string,
-  radios: {
+  radio: {
     id: string,
     label: string,
     value: string,
-  }[]
+  }
 }>()
 </script>
 

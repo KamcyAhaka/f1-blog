7647b4daf7b80475781ca9c5c5b1b9c5a3f4e6ba
KamCy Ahaka <kamcyahaka@gmail.com>
2023-11-16 22:17:22 +0100

    💄Began creating blog creation page

--- components/TipTap.vue
+++ components/TipTap.vue
@@ -1,10 +1,146 @@
 <template>
+  <div
+    class="flex gap-2 flex-wrap bg-gray-200 rounded-lg p-3 shadow-md"
+    v-if="editor"
+  >
+    <button
+      @click="editor.chain().focus().toggleBold().run()"
+      :disabled="!editor.can().chain().focus().toggleBold().run()"
+      :class="{ 'is-active': editor.isActive('bold') }"
+    >
+      <client-only>
+        <font-awesome-icon :icon="['fas', 'bold']" />
+      </client-only>
+    </button>
+    <button
+      @click="editor.chain().focus().toggleItalic().run()"
+      :disabled="!editor.can().chain().focus().toggleItalic().run()"
+      :class="{ 'is-active': editor.isActive('italic') }"
+    >
+      <client-only>
+        <font-awesome-icon :icon="['fas', 'italic']" />
+      </client-only>
+    </button>
+    <button
+      @click="editor.chain().focus().toggleStrike().run()"
+      :disabled="!editor.can().chain().focus().toggleStrike().run()"
+      :class="{ 'is-active': editor.isActive('strike') }"
+    >
+      <client-only>
+        <font-awesome-icon :icon="['fas', 'strikethrough']" />
+      </client-only>
+    </button>
+    <button
+      @click="editor.chain().focus().toggleCode().run()"
+      :disabled="!editor.can().chain().focus().toggleCode().run()"
+      :class="{ 'is-active': editor.isActive('code') }"
+    >
+      <client-only>
+        <font-awesome-icon :icon="['fas', 'code']" />
+      </client-only>
+    </button>
+    <button @click="editor.chain().focus().unsetAllMarks().run()">
+      <client-only>
+        <font-awesome-icon :icon="['fas', 'eraser']" />
+      </client-only>
+    </button>
+    <button
+      @click="editor.chain().focus().setParagraph().run()"
+      :class="{ 'is-active': editor.isActive('paragraph') }"
+    >
+      <client-only>
+        <font-awesome-icon :icon="['fas', 'paragraph']" />
+      </client-only>
+    </button>
+    <button
+      @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
+    >
+      h1
+    </button>
+    <button
+      @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
+    >
+      h2
+    </button>
+    <button
+      @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
+    >
+      h3
+    </button>
+    <button
+      @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }"
+    >
+      h4
+    </button>
+    <button
+      @click="editor.chain().focus().toggleHeading({ level: 5 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 5 }) }"
+    >
+      h5
+    </button>
+    <button
+      @click="editor.chain().focus().toggleHeading({ level: 6 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 6 }) }"
+    >
+      h6
+    </button>
+    <button
+      @click="editor.chain().focus().toggleBulletList().run()"
+      :class="{ 'is-active': editor.isActive('bulletList') }"
+    >
+      <client-only>
+        <font-awesome-icon :icon="['fas', 'list']" />
+      </client-only>
+    </button>
+    <button
+      @click="editor.chain().focus().toggleOrderedList().run()"
+      :class="{ 'is-active': editor.isActive('orderedList') }"
+    >
+      ordered list
+    </button>
+    <button
+      @click="editor.chain().focus().toggleCodeBlock().run()"
+      :class="{ 'is-active': editor.isActive('codeBlock') }"
+    >
+      code block
+    </button>
+    <button
+      @click="editor.chain().focus().toggleBlockquote().run()"
+      :class="{ 'is-active': editor.isActive('blockquote') }"
+    >
+      blockquote
+    </button>
+    <button @click="editor.chain().focus().setHorizontalRule().run()">
+      horizontal rule
+    </button>
+    <button @click="editor.chain().focus().setHardBreak().run()">
+      hard break
+    </button>
+    <button
+      @click="editor.chain().focus().undo().run()"
+      :disabled="!editor.can().chain().focus().undo().run()"
+    >
+      undo
+    </button>
+    <button
+      @click="editor.chain().focus().redo().run()"
+      :disabled="!editor.can().chain().focus().redo().run()"
+    >
+      redo
+    </button>
+  </div>
   <editor-content :editor="editor" />
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { useEditor, EditorContent } from '@tiptap/vue-3'
 import StarterKit from '@tiptap/starter-kit'
+
+
 
 const editor = useEditor({
   content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
@@ -13,3 +149,62 @@ const editor = useEditor({
   ],
 })
 </script>
+
+<style lang="scss">
+/* Basic editor styles */
+.tiptap {
+  > * + * {
+    margin-top: 0.75em;
+  }
+
+  ul,
+  ol {
+    padding: 0 1rem;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    line-height: 1.1;
+  }
+
+  code {
+    background-color: rgba(#616161, 0.1);
+    color: #616161;
+  }
+
+  pre {
+    background: #0D0D0D;
+    color: #FFF;
+    font-family: 'JetBrainsMono', monospace;
+    padding: 0.75rem 1rem;
+    border-radius: 0.5rem;
+
+    code {
+      color: inherit;
+      padding: 0;
+      background: none;
+      font-size: 0.8rem;
+    }
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  blockquote {
+    padding-left: 1rem;
+    border-left: 2px solid rgba(#0D0D0D, 0.1);
+  }
+
+  hr {
+    border: none;
+    border-top: 2px solid rgba(#0D0D0D, 0.1);
+    margin: 2rem 0;
+  }
+}
+</style>
---
+++ pages/admin/blogs/new.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtLayout
+    name="form-layout"
+    form-header="Create a new blog post"
+  >
+    <TipTap class="w-[80vw]" />
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+</style>

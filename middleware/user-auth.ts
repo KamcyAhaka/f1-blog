b7aaf117a916ddd3753df33c0934d11c61bd6c07
KamCy Ahaka <kamcyahaka@gmail.com>
2024-03-07 23:31:33 +0100

    feat(middleware/user-auth.ts): update admin route guard to redirect to signin page not auth index

--- middleware/user-auth.ts
+++ middleware/user-auth.ts
@@ -2,7 +2,10 @@ import { useUserStore } from '~/stores/user';
 
 export default defineNuxtRouteMiddleware((to, from) => {
   const userStore = useUserStore();
+
   if (userStore.user === null) {
-    return navigateTo('/auth/');
+    return navigateTo(
+      `/auth/signin?redirect=${encodeURIComponent(from?.path)}`
+    );
   }
 });

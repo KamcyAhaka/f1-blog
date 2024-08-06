87c42dd260176313675e767873cfd8bc0fcfc861
KamCy Ahaka <kamcyahaka@gmail.com>
2024-08-06 20:43:38 +0100

    style(toast.vue): change Toast design

--- components/Toast.vue
+++ components/Toast.vue
@@ -1,21 +1,29 @@
 <template>
-  <div
-    class="toast-notification w-full fixed top-5 right-0 min-h-[8vh] max-w-sm mx-auto text-white flex rounded-s-2xl shadow-lg"
-    :class="props.toast.type === 'success' ? 'bg-green-500' : props.toast.type === 'error' ? 'bg-red-500' : props.toast.type === 'warning' ? 'bg-yellow-500' : ''"
-  >
-    <div class="toast-notification-icon flex items-center px-5 border-r-2">
-      <ClientOnly>
-        <font-awesome-icon
-          class="text-3xl"
-          :icon="props.toast.type === 'success' ? ['fas', 'check'] : props.toast.type === 'error' ? ['fas', 'xmark'] : props.toast.type === 'warning' ? ['fas', 'triangle-exclamation'] : ''"
-        />
-      </ClientOnly>
+  <div class="toast-container flex justify-center items-center fixed z-50 top-5 w-full min-h-[3vh] min:max-h-[5vh]">
+    <div
+      class="toast-notification w-[90%] mx-auto max-w-3xl h-full flex overflow-hidden rounded-2xl shadow-lg bg-white"
+      :class="props.toast.type === 'success' ? 'border-green-500 border-2' : props.toast.type === 'error' ? 'border-red-500 border-2' : props.toast.type === 'warning' ? 'border-yellow-500 border-2' : ''"
+    >
+      <div
+        class="toast-notification-icon flex items-center px-5 border-r-2 text-white"
+        :class="props.toast.type === 'success' ? 'bg-green-500' : props.toast.type === 'error' ? 'bg-red-500' : props.toast.type === 'warning' ? 'bg-yellow-500' : ''"
+      >
+        <ClientOnly>
+          <font-awesome-icon
+            class="text-xl md:text-3xl"
+            :icon="props.toast.type === 'success' ? ['fas', 'check'] : props.toast.type === 'error' ? ['fas', 'xmark'] : props.toast.type === 'warning' ? ['fas', 'triangle-exclamation'] : ''"
+          />
+        </ClientOnly>
+      </div>
+      <div class="toast-notification-text flex items-center p-3 ">{{ props.toast.text }}</div>
     </div>
-    <div class="toast-notification-text flex items-center px-5 py-3">{{ props.toast.text }}</div>
   </div>
 </template>
 
-<script setup lang="ts">
+<script
+  setup
+  lang="ts"
+>
 import type ToastType from '~/types/Toast'
 
 const props = defineProps<{
@@ -23,5 +31,7 @@ const props = defineProps<{
 }>()
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style
+  lang="scss"
+  scoped
+></style>
